c551ab18e88218e8316b342f3da75470209e19e9
dan0666 <106615822+dan0666@users.noreply.github.com>
2024-09-10 19:57:30 +0300

    Updated server.lua

--- server/server.lua
+++ server/server.lua
@@ -1,20 +1,24 @@
-local weaponChances = Config.WeaponChances or {}
-local baseChance = Config.BaseChance or 100
-
 RegisterServerEvent('knockout:checkDamage')
 AddEventHandler('knockout:checkDamage', function(targetPlayer, damage, weaponHash)
     if damage > 0 then
-        local weaponChance = weaponChances[weaponHash] or baseChance
-        
+        local knockoutChance = Config.BaseChance
+        if Config.WeaponChances[weaponHash] then
+            knockoutChance = Config.WeaponChances[weaponHash]
+        end
+
+        knockoutChance = math.max(0, math.min(100, knockoutChance))
+
         local chance = math.random(1, 100)
-        
-        if chance <= weaponChance then
+
+        if chance <= knockoutChance then
             TriggerClientEvent('knockout:start', targetPlayer)
         end
     end
 end)
 
+-- Synchronize player standing up across all clients
 RegisterServerEvent('knockout:syncStandUp')
 AddEventHandler('knockout:syncStandUp', function(playerId)
+-- Broadcast to all players so that everyone sees the player getting up normally
     TriggerClientEvent('knockout:doStandUp', -1, playerId)
 end)
